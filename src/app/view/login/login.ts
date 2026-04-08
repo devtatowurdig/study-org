5fc5c4299edd15c8ca7c719ccd7fd39afb9b3867
Tácio <email.tato@gmail.com>
2026-04-08 20:37:52 -0300

    🚧 Atualiza Tela de Login

--- src/app/view/login/login.ts
+++ src/app/view/login/login.ts
@@ -2,9 +2,12 @@ import { Component } from '@angular/core';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { faArrowRight, faEnvelope, faLock } from '@fortawesome/free-solid-svg-icons';
 import { Logo } from '../../shared/components/logo/logo';
+import { Entrar } from "../../shared/components/entrar/entrar";
+import { NgIf, NgClass } from "@angular/common";
+import { Registrar } from "../../shared/components/registrar/registrar";
 @Component({
   selector: 'app-login',
-  imports: [Logo, FontAwesomeModule],
+  imports: [Logo, FontAwesomeModule, Entrar, NgIf, Registrar, NgClass],
   templateUrl: './login.html',
   styleUrl: './login.css',
 })
@@ -12,5 +15,11 @@ export class Login {
   logo = Logo;
   emailIcon = faEnvelope;
   passwordIcon = faLock;
-  arrowIcon = faArrowRight
+  arrowIcon = faArrowRight;
+  
+  activeTab: 'entrar' | 'cadastrar' = 'entrar';
+
+  switchTab(tab: 'entrar' | 'cadastrar') {
+    this.activeTab = tab;
+  }
 }

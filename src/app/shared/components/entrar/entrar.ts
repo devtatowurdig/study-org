204bbf9cb520cacc396d7691b5ddbcd77c4ad27d
Felipe Rosso <felipe_rosso123@hotmail.com>
2026-04-12 13:04:22 -0300

    🐛 Ajuste no botao de entrar na tela de login

--- src/app/shared/components/entrar/entrar.ts
+++ src/app/shared/components/entrar/entrar.ts
@@ -1,5 +1,6 @@
-import { Component, Input } from '@angular/core';
+import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { Router } from '@angular/router';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { IconDefinition } from '@fortawesome/fontawesome-svg-core';
 import { faArrowRight, faEnvelope, faLock } from '@fortawesome/free-solid-svg-icons';
@@ -12,8 +13,14 @@ import { InputLogin } from "../input-login/input-login";
   styleUrl: './entrar.css',
 })
 export class Entrar {
+  constructor(private readonly router: Router) {}
+
   emailIcon = faEnvelope;
-  passwordIcon = faLock;
   arrowIcon = faArrowRight;
   lockIcon: IconDefinition = faLock;
+
+  onSubmit(event: Event): void {
+    event.preventDefault();
+    void this.router.navigate(['/dashboard']);
+  }
 }
